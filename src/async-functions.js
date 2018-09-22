a75fbcaaf2949e84aaf856a8fa4077ed1f24bdba
VosKoen <Koen82@gmail.com>
2018-09-22 14:14:55 +0200

    finished assignment 3, all three assignments are done

--- src/async-functions.js
+++ src/async-functions.js
@@ -5,12 +5,16 @@ function giveItBackLater(value, callback) {
   setTimeout(waitWithReturn, 500);
 }
 
-function addSomePromises() {}
+function addSomePromises(somePromise) {
+return somePromise.then(value => value + value).catch(value => value + value + value)
+}
 
 function promiseToGiveItBackLater(value) {
   return new Promise((resolve, reject) => {
-    resolve(setTimeout((value) => value, 500));
+    giveItBackLater(value, resolve);
   });
 }
+
+
 
 module.exports = { giveItBackLater, addSomePromises, promiseToGiveItBackLater };
